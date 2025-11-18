3aa04d930d24f4e5dab97393f17cf9681b000eb4
raffael-carvalho <ffaelcdec@gmail.com>
2025-11-18 15:53:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Separamos a lógica do app da inicialização do servidor
+// para que os testes possam carregar o app sem iniciar o servidor.
+//app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Olá Mundo DevSecOps!");
+});
+
+module.exports = app; // Exportamos o app
